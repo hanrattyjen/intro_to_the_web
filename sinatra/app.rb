d55885900b154efe765b14865dc3e584d5c0fb57
souljuse <oppo.irene@gmail.com>
2016-10-10 16:01:23 +0200

    installed shotgun that reloads sessions

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,1 +1,19 @@
 require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  'This is so secret it does not even exist'
+end
+
+get '/jennifer' do
+  'Hi Jennifer!'
+end
+
+get '/ciao' do
+  'Ciao'
+end
