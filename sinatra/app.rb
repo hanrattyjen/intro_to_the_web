066a489a858e87bee3c05fdaad8706cbbe985435
souljuse <oppo.irene@gmail.com>
2016-10-10 16:46:36 +0200

    created a views folder and an index file

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,25 +1,23 @@
 require 'sinatra'
 
-set :session_secret, 'super secret'
+  set :session_secret, 'super secret'
 
-get '/' do
-  'Hello world!'
-end
+  get '/' do
+    'Hello world!'
+  end
 
-get '/secret' do
-  'This is so secret it does not even exist'
-end
+  get '/secret' do
+    'This is so secret it does not even exist'
+  end
 
-get '/jennifer' do
-  'Hi Jennifer!'
-end
+  get '/jennifer' do
+    'Hi Jennifer!'
+  end
 
-get '/ciao' do
-  'Ciao'
-end
+  get '/ciao' do
+    'Ciao'
+  end
 
-get '/cat' do
-  '<div>
-  <img style="border: 2px dashed red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">
-  </div>'
-end
+  get '/cat' do
+    erb(:index)
+  end
